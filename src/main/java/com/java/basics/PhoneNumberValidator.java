e7969a8c5e625e9b1a312d4ef10dbf0c89fa0cec
mybear517 <mybear517@gmail.com>
2021-03-02 18:28:16 -0500

    updated

--- src/main/java/com/java/basics/PhoneNumberValidator.java
+++ src/main/java/com/java/basics/PhoneNumberValidator.java
@@ -1,17 +1,18 @@
 package com.java.basics;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneNumberValidator {
     //Create Scanner object as instance variable
-	public static Scanner in = new Scanner(System.in);
+	public static InputStreamReader input = new InputStreamReader(System.in);
 
     public static void main(String[] args) {
         //call the functions in the required sequence
-    	 System.out.println("Enter a valid Phone Number (999)999-9999:");
-  	     String phone = getInput();
+        String phone = getInput();
   	
          int i = validatePhoneNumber(phone);
    	 
@@ -21,34 +22,67 @@ public class PhoneNumberValidator {
     }
 
     public static String getInput() {
-        return in.nextLine();
+        BufferedReader br = new BufferedReader(input);
+        String sInput = null;
+        try {
+			sInput = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        return sInput;
     }
 
     public static void displayResult(int result) {
         //displays the result
-    	System.out.println(result);
+    	if(result == 1) {
+    		System.out.println("VALID");
+    	} else if(result == -1) {
+    		System.out.println("EMPTY STRING");
+    	} else {
+    		System.out.println("INVALID");
+    	}
+    		
     }
-
+  
     public static int validatePhoneNumber(String input) {
-    	 
-    	if (input == null) {
-             return -1;
+    	
+    	if (input == null || input.trim().length() == 0) {
+            return -1;
+    	}
+    	Pattern p = Pattern.compile("^([0-9]+-)*[0-9]+$");
+    	Matcher m = p.matcher(input);
+    	int numCount = 0;
+    	
+    	for(int i = 0; i < input.length();i++)
+        {
+        	if(Character.isDigit(input.charAt(i)) == true)
+        	{
+        		numCount++;
+        	}
+        }
+        
+        if(numCount < 10 || numCount > 10)
+        {
+        	return 0;
+        }
+    	
+
+         if(m.matches()) {
+        	 return 1;
+         } else {
+        	 return 0;
          }
-         Pattern p = Pattern.compile("\\(\\d{3}\\)-\\d{3}-\\d{4}"); 
-         for (int i = 0; i < input.length(); i++) {  
-        	 String lc = String.valueOf(input.charAt(i));
-        	 Matcher m = p.matcher(lc);
-        	 if(m.matches()) {
-        		 return 1;
-        	 } else {
-        		 return 0;
-        	 }
-        		 
-         }
-        return 0;
+      
     }
+ 
     public static void closeScanner(){
-        //close the Scanner object
-    	in.close();
+        //close the Scanner object 
+    	System.out.println("Scanner Close");
+    	try {
+			input.close();
+		} catch (IOException e) {
+            System.out.println("scanner exception");
+			e.printStackTrace();
+		}
     }
 }
